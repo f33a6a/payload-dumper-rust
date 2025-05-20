874e47562a3c5d5edaa4751e902880fc9b40aaa9
Calm <omp803113@proton.me>
2025-05-20 10:23:43 +0530

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,25 +1,31 @@
 #[cfg(windows)]
 fn find_libzip(use_static: bool) {
     let mut config = vcpkg::Config::new();
+
     if use_static {
         config.r#static(true);
+        println!("cargo:rustc-link-lib=static=zip");
+    } else {
+        println!("cargo:rustc-link-lib=zip");
     }
-    config.find_package("libzip").expect("Failed to find libzip via vcpkg");
+
+    config
+        .find_package("libzip")
+        .expect("Failed to find libzip via vcpkg");
 }
 
 #[cfg(not(windows))]
 fn find_libzip(use_static: bool) {
     let mut config = pkg_config::Config::new();
+
     if use_static {
         config.statik(true);
+        println!("cargo:rustc-link-lib=static=zip");
+    } else {
+        println!("cargo:rustc-link-lib=zip");
     }
-    config.probe("libzip").expect("Failed to find libzip via pkg-config");
-}
 
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    let use_static =
-        std::env::var("CARGO_FEATURE_STATIC").is_ok() || std::env::var("LIBZIP_STATIC").is_ok();
-
-    find_libzip(use_static);
+    config
+        .probe("libzip")
+        .expect("Failed to find libzip via pkg-config");
 }
